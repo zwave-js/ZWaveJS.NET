bfd581407870e2baba27f506892f6d1d84667c80
Marcus Davies <55892693+marcus-j-davies@users.noreply.github.com>
2021-12-19 14:25:53 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+const { Driver, ZWaveError, ZWaveErrorCodes } = require("zwave-js");
+const Server = require("@zwave-js/server");
 
-const { Driver, ZWaveError, ZWaveErrorCodes } = require("zwave-js")
-const Server = require("@zwave-js/server")
+console.log('ZWaveJS.NET: Preparing server...');
 
 let WSServer;
 
 const SieralPort = process.env.SERIAL_PORT;
 const WSPort = parseInt(process.env.WS_PORT);
-const DriverOptions = JSON.parse(process.env.CONFIG)
+const DriverOptions = JSON.parse(process.env.CONFIG);
+
+console.log(`ZWaveJS.NET: Serial Port: ${SieralPort}, WSPort: ${WSPort}`);
 
 if (DriverOptions.securityKeys) {
     for (const key in DriverOptions.securityKeys) {
@@ -14,6 +17,7 @@ if (DriverOptions.securityKeys) {
     }
 }
 
+console.log('ZWaveJS.NET: Instantiating Driver...');
 const _Driver = new Driver(SieralPort, DriverOptions);
 WSServer = new Server.ZwavejsServer(_Driver, {port: WSPort, host: 'localhost'})
 _Driver.on('error', (e) => {
@@ -26,10 +30,8 @@ _Driver.on("driver ready", () => {
     WSServer.start();
 })
 
+console.log('ZWaveJS.NET: Starting Driver...');
 _Driver.start()
     .catch((e) => {
         process.exit(0);
-    })
-    .then(() => {
-
-    })
+    });
