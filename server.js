4db5255515e4d76689d6a65147fd6a65a89ce7fe
AlCalzone <d.griesel@gmx.net>
2021-12-20 21:41:22 +0100

    refactor: some cleanup (#5)

--- server.js
+++ server.js
@@ -1,37 +1,43 @@
+// @ts-check
+
 const { Driver, ZWaveError, ZWaveErrorCodes } = require("zwave-js");
-const Server = require("@zwave-js/server");
+const { ZwavejsServer } = require("@zwave-js/server");
 
-console.log('ZWaveJS.NET: Preparing server...');
+console.log("ZWaveJS.NET: Preparing server...");
 
-let WSServer;
+const serialPort = process.env.SERIAL_PORT;
+const wsPort = parseInt(process.env.WS_PORT);
+/** @type {import("zwave-js").ZWaveOptions} */
+const driverOptions = JSON.parse(process.env.CONFIG);
 
-const SieralPort = process.env.SERIAL_PORT;
-const WSPort = parseInt(process.env.WS_PORT);
-const DriverOptions = JSON.parse(process.env.CONFIG);
+console.log(`ZWaveJS.NET: Serial Port: ${serialPort}, WSPort: ${wsPort}`);
 
-console.log(`ZWaveJS.NET: Serial Port: ${SieralPort}, WSPort: ${WSPort}`);
-
-if (DriverOptions.securityKeys) {
-    for (const key in DriverOptions.securityKeys) {
-        DriverOptions.securityKeys[key] = Buffer.from(DriverOptions.securityKeys[key], 'hex');
+if (driverOptions.securityKeys) {
+    for (const key of Object.keys(driverOptions.securityKeys)) {
+        driverOptions.securityKeys[key] = Buffer.from(
+            driverOptions.securityKeys[key],
+            "hex"
+        );
     }
 }
 
-console.log('ZWaveJS.NET: Instantiating Driver...');
-const _Driver = new Driver(SieralPort, DriverOptions);
-WSServer = new Server.ZwavejsServer(_Driver, port: {port: WSPort, host: 'localhost'})
-_Driver.on('error', (e) => {
+console.log("ZWaveJS.NET: Instantiating driver...");
+const driver = new Driver(serialPort, driverOptions);
+// @ts-expect-error The host property is a workaround
+const server = new ZwavejsServer(driver, { port: wsPort, host: "localhost" });
+driver.on("error", (e) => {
+    console.error(e);
     if (e instanceof ZWaveError && e.code === ZWaveErrorCodes.Driver_Failed) {
-        process.exit(0);
+        process.exit(2); // Exit code 2: restart requested
     }
-})
+});
 
-_Driver.on("driver ready", () => {
-    WSServer.start();
-})
+driver.on("driver ready", () => {
+    server.start();
+});
 
-console.log('ZWaveJS.NET: Starting Driver...');
-_Driver.start()
-    .catch((e) => {
-        process.exit(0);
-    });
+console.log("ZWaveJS.NET: Starting driver...");
+driver.start().catch((e) => {
+    console.error(e);
+    process.exit(1); // Exit code 1: cannot start
+});
