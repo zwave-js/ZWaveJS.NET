08bca144492fdecf58de5b34992c19f6675f81e0
Marcus Davies <55892693+marcus-j-davies@users.noreply.github.com>
2022-06-08 10:09:14 +0100

    Update server.js

--- PSI/server.js
+++ PSI/server.js
@@ -6,6 +6,8 @@ console.log("ZWaveJS.NET: Preparing server...");
 const serialPort = process.env.SERIAL_PORT;
 const wsPort = parseInt(process.env.WS_PORT);
 const driverOptions = JSON.parse(process.env.CONFIG);
+let ServerStarted = false;
+let DriverStarted = false;
 
 console.log(`ZWaveJS.NET: Serial Port: ${serialPort}, WSPort: ${wsPort}`);
 
@@ -21,25 +23,31 @@ if (driverOptions.securityKeys) {
 console.log("ZWaveJS.NET: Instantiating driver...");
 const driver = new Driver(serialPort, driverOptions);
 const server = new ZwavejsServer(driver, { port: wsPort, host: "localhost" });
+server.on("listening",() =>{
+    ServerStarted = true;
+})
 driver.on("error", (e) => {});
 
 driver.on("driver ready", () => {
     server.start();
+    ServerStarted = true;
 });
 
 console.log("ZWaveJS.NET: Starting driver...");
 driver.start()
 .then(() =>{
-    process.on("beforeExit",CleanUp);
+    DriverStarted = true;
+    process.stdin.on("data",HandleInput)
 })
 .catch((e) => {
     process.stderr.write("1\n");
 })
 
-const CleanUp = async (code) =>{
-    console.log("ZWaveJS.NET: Cleaning up...");
-    await server.destroy();
-    await driver.destroy();
-    process.exit(0);
-
+const HandleInput = async (Data) =>{
+    if(Data.toString() === "KILL"){
+        console.log("ZWaveJS.NET: Cleaning up...");
+        if(ServerStarted) await server.destroy();
+        if(DriverStarted) await driver.destroy();
+        process.exit(0);
+    }
 }
